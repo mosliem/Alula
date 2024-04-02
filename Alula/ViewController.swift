f9111cc43c2ae0149f95afb60bb5d0b7788b874d
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2024-04-03 00:17:32 +0200

    Initial Commit

---
+++ Alula/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alula
+//
+//  Created by mohamed sliem on 03/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
