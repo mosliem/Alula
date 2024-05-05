794a479d1aea063cb54fc36bc74a7742c7fe1267
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2024-05-05 04:30:28 +0300

    Added Crashlytics

--- Alula/Core/AppDelegate.swift
+++ Alula/Core/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        FirebaseApp.configure()
         return true
     }
     // MARK: UISceneSession Lifecycle
